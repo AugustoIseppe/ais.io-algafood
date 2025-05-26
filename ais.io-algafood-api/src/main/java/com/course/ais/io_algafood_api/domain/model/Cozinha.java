2e4f9f37176d42d1c543d7239b994da47bf8bebf
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-26 11:35:28 -0300

    feat: mapeamento das entidades Cozinha e Restaurante

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Cozinha.java
@@ -0,0 +1,46 @@
+package com.course.ais.io_algafood_api.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tab_cozinhas")
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom_cozinha", nullable = false)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
