3a45fe86ffad23251b14e61a71863d3397407282
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-26 13:51:45 -0300

    feat: adicionando um objetos do banco de dados

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/jpa/InclusaoCozinhaMain.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/jpa/InclusaoCozinhaMain.java
@@ -2,14 +2,13 @@ package com.course.ais.io_algafood_api.jpa;
 
 import com.course.ais.io_algafood_api.AlgafoodApplication;
 import com.course.ais.io_algafood_api.domain.model.Cozinha;
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.List;
 
-public class ConsultaCozinhaMain {
+public class InclusaoCozinhaMain {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApplication.class)
@@ -17,10 +16,16 @@ public class ConsultaCozinhaMain {
                 .run(args);
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
-        List<Cozinha> cozinhas = cadastroCozinha.listar();
-        for (Cozinha cozinha : cozinhas) {
-            System.out.printf("%d - %s\n", cozinha.getId(), cozinha.getNome());
-        }
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Tailandesa");
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Americana");
+
+        cadastroCozinha.adicionar(cozinha1);
+        cadastroCozinha.adicionar(cozinha2);
+
+        System.out.printf( "%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+        System.out.printf( "%d - %s\n", cozinha2.getId(), cozinha2.getNome());
 
     }
 }
