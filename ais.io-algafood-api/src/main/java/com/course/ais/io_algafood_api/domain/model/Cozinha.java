486999c4aa1af572c26b27607d0ba92c061cd323
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-27 08:57:20 -0300

    refactor: alterando o imports do lombok e a anotation para @Data

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Cozinha.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Cozinha.java
@@ -3,10 +3,6 @@ package com.course.ais.io_algafood_api.domain.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Objects;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
