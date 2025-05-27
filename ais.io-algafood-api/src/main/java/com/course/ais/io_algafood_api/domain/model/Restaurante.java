1343bcb3ec812436d48b22a7477802e32f77d551
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-27 08:57:56 -0300

    feat: inserido o relacionamento many to one

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Restaurante.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Restaurante.java
@@ -3,11 +3,8 @@ package com.course.ais.io_algafood_api.domain.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -23,4 +20,7 @@ public class Restaurante {
 
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
+
+    @ManyToOne
+    private Cozinha cozinha;
 }
