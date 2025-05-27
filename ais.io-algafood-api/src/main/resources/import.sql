7fbf78a40d1ad9fc67b50eaa94c383c356532ea4
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-27 08:56:11 -0300

    build: inserindo restaurantes no seeding

--- ais.io-algafood-api/src/main/resources/import.sql
+++ ais.io-algafood-api/src/main/resources/import.sql
@@ -1,4 +1,6 @@
-insert into cozinha (nome) values ('Brasileira');
-insert into cozinha (nome) values ('Japonesa');
-insert into cozinha (nome) values ('Francesa');
+insert into cozinha (id, nome) values (1, 'Tailandesa');
+insert into cozinha (id, nome) values (2, 'Indiana');
 
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet 1', 10.00, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery 2', 15.00, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida 3', 20.00, 2);
