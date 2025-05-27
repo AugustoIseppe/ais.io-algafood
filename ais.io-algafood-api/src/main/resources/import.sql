14a44265e2bbb53f7b835cf77d643abc1ad1697e
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-27 09:19:22 -0300

    chore: seeding FormaPagamento

--- ais.io-algafood-api/src/main/resources/import.sql
+++ ais.io-algafood-api/src/main/resources/import.sql
@@ -4,3 +4,5 @@ insert into cozinha (id, nome) values (2, 'Indiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet 1', 10.00, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery 2', 15.00, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida 3', 20.00, 2);
+
+insert into forma_pagamento (id, descricao) values (1, 'Dinheiro');
