5c0fe3e35f514061577d26b051f8e988c33c0b00
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-27 18:16:00 -0300

    feat: Wrapper da cozinha para retorno do tipo xml

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/controller/CozinhaController.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/controller/CozinhaController.java
@@ -1,8 +1,13 @@
 package com.course.ais.io_algafood_api.api.controller;
 
+import com.course.ais.io_algafood_api.api.model.CozinhaXmlWrapper;
 import com.course.ais.io_algafood_api.domain.model.Cozinha;
 import com.course.ais.io_algafood_api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +23,25 @@ public class CozinhaController {
     private CozinhaRepository cozinhaRepository;
 
     @GetMapping
-    public List<Cozinha> listar(){
+    public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }
 
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhaXmlWrapper listarXML() {
+        return new CozinhaXmlWrapper(cozinhaRepository.listar());
+    }
+
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .header(HttpHeaders.WARNING, "Cozinha não encontrada")
+                    .build();
+        }
+
     }
 }
