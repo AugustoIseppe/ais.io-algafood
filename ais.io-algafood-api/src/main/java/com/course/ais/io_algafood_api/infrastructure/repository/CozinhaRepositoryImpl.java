56f1d769fd151b43f17f305091add221b937fce1
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-28 16:28:01 -0300

    feat: consultar por nome

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/infrastructure/repository/CozinhaRepositoryImpl.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -7,11 +7,12 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
     @PersistenceContext
@@ -21,6 +22,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
     public List<Cozinha> listar() {
         TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
         return query.getResultList();
+    }
+
+    @Override
+    public List<Cozinha> consultarPorNome(String nome) {
+        return manager.createQuery("from Cozinha where nome like = :nome", Cozinha.class)
+                .setParameter("nome", "%" + nome + "%")
+                .getResultList();
     }
 
     @Override
