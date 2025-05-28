37fae463854b6d75a7cd5b18105bf66d4e144f5e
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-28 10:16:33 -0300

    feat: implementando exceptions (EntidadeEmUsoException/EntidadeNaoEncontradaException)

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/exceptions/EntidadeEmUsoException.java
@@ -0,0 +1,10 @@
+package com.course.ais.io_algafood_api.domain.exceptions;
+
+public class EntidadeEmUsoException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public EntidadeEmUsoException(String mensagem) {
+        super(mensagem);
+    }
+}
