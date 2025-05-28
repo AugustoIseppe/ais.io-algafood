2fe83ac6edc6507de325d43be90e8aed22fc878a
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-28 10:17:41 -0300

    feat: CadastroCozinhaService

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,34 @@
+package com.course.ais.io_algafood_api.domain.service;
+
+import com.course.ais.io_algafood_api.domain.exceptions.EntidadeEmUsoException;
+import com.course.ais.io_algafood_api.domain.exceptions.EntidadeNaoEncontradaException;
+import com.course.ais.io_algafood_api.domain.model.Cozinha;
+import com.course.ais.io_algafood_api.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCozinhaService {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Cozinha salvar(Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
+    }
+
+    public void excluir(Long cozinhaId) {
+        try {
+            cozinhaRepository.remover(cozinhaId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Cozinha de código %d não encontrada", cozinhaId));
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
+        }
+    }
+
+}
