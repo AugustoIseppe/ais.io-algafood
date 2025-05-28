1710002561f4651aa97d2891135c4ce5232ffb00
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-28 16:40:14 -0300

    refactor: migrando para JpaRepository

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/CozinhaRepository.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/CozinhaRepository.java
@@ -1,13 +1,14 @@
 package com.course.ais.io_algafood_api.domain.repository;
 
 import com.course.ais.io_algafood_api.domain.model.Cozinha;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface CozinhaRepository {
-    List<Cozinha> listar();
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+
     List<Cozinha> consultarPorNome(String nome);
-    Cozinha buscar(Long id);
-    Cozinha salvar(Cozinha cozinha);
-    void remover(Long id);
+
 }
