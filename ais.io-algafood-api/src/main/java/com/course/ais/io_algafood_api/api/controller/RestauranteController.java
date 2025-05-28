c15bbab9fe11968e15d192bb3b2a0667043f2e54
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-28 13:35:28 -0300

    feat: implementando o atualizar parcial com patch

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/controller/RestauranteController.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/controller/RestauranteController.java
@@ -4,13 +4,18 @@ import com.course.ais.io_algafood_api.domain.exceptions.EntidadeNaoEncontradaExc
 import com.course.ais.io_algafood_api.domain.model.Restaurante;
 import com.course.ais.io_algafood_api.domain.repository.RestauranteRepository;
 import com.course.ais.io_algafood_api.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -54,12 +59,49 @@ public class RestauranteController {
     @PutMapping("/{restauranteId}")
     public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
         try {
-            restaurante.setId(restauranteId);
-            restaurante = cadastroRestauranteService.salvar(restaurante);
-            return ResponseEntity.ok(restaurante);
+            Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+
+            if (restauranteAtual != null) {
+                BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+                restauranteAtual = cadastroRestauranteService.salvar(restauranteAtual);
+                return ResponseEntity.ok(restauranteAtual);
+            }
+
+            return ResponseEntity.notFound().build();
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest()
                     .body(e.getMessage());
         }
+    }
+
+    @PatchMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+
+        Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+
+        if (restauranteAtual == null) {
+            return ResponseEntity.notFound().build();
+        }
+        merge(campos, restauranteAtual);
+        return atualizar(restauranteId, restauranteAtual);
+    }
+
+    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            System.out.println("Propriedade: " + nomePropriedade + ", Valor: " + valorPropriedade);
+            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+            if (field != null) {
+
+                field.setAccessible(true);
+                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+                ReflectionUtils.setField(field, restauranteDestino, novoValor);
+            } else {
+                throw new IllegalArgumentException("Propriedade " + nomePropriedade + " não existe na classe Restaurante");
+            }
+        });
     }
 }
