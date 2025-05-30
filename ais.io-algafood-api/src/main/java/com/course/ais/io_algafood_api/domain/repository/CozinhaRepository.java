35600058a15eb05c483ccf71f8b2ca432f2e4444
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-30 10:05:23 -0300

    feat: migrando para JpaRepository (nao testado)

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/CozinhaRepository.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/CozinhaRepository.java
@@ -4,7 +4,11 @@ import com.course.ais.io_algafood_api.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
-
+    List<Cozinha> findTodasByNomeContaining(String nome);
+    Optional<Cozinha> findByNome(String nome);
 }
--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/RestauranteRepository.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/RestauranteRepository.java
@@ -3,5 +3,10 @@ package com.course.ais.io_algafood_api.domain.repository;
 import com.course.ais.io_algafood_api.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 }
