1dcb0eb4ddf41bc44c768660f51cb57e5f288192
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-30 16:30:32 -0300

    feat: json ignore

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Restaurante.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Restaurante.java
@@ -1,5 +1,6 @@
 package com.course.ais.io_algafood_api.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -27,6 +28,7 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
