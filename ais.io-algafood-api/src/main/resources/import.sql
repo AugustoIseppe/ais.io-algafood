f2d7c166717998e676fa334e6bd18e138e67cfa8
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-30 17:30:16 -0300

    refactor: update seeding db

--- ais.io-algafood-api/src/main/resources/import.sql
+++ ais.io-algafood-api/src/main/resources/import.sql
@@ -11,9 +11,9 @@ insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
 insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
-insert into restaurante (nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values ('Thai Gourmet 1', 10.00, 1, 3, '38400-000', 'Rua das Flores', '123', 'Centro');
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery 2', 15.00, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida 3', 20.00, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values ('Thai Gourmet 1', 10.00, 1, utc_timestamp, utc_timestamp, 3, '38400-000', 'Rua das Flores', '123', 'Centro');
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ('Thai Delivery 2', 15.00, 1, utc_timestamp, utc_timestamp);
+insert into restaurante (nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values ('Tuk Tuk Comida 3', 20.00, 2, utc_timestamp, utc_timestamp);
 
 insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
 insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
