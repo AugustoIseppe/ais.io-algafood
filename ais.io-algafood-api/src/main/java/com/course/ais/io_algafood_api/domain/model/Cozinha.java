d37ba662b99d937d91f9ec40484a77e5ea5c5d20
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-30 15:23:12 -0300

    feat: mepeando relacionamento bidirecional com OneToMany

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Cozinha.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/model/Cozinha.java
@@ -1,8 +1,12 @@
 package com.course.ais.io_algafood_api.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -16,4 +20,8 @@ public class Cozinha {
 
     @Column(name = "nome")
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
 }
