2b825fa567d12b895c184bb7d4f762eef362db91
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-03 14:18:23 -0300

    feat: melhorando o exception handler

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/exceptionhandler/ApiExceptinHandler.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/exceptionhandler/ApiExceptinHandler.java
@@ -3,11 +3,13 @@ package com.course.ais.io_algafood_api.api.exceptionhandler;
 import com.course.ais.io_algafood_api.domain.exceptions.EntidadeEmUsoException;
 import com.course.ais.io_algafood_api.domain.exceptions.EntidadeNaoEncontradaException;
 import com.course.ais.io_algafood_api.domain.exceptions.NegocioException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -15,31 +17,44 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class ApiExceptinHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
-    }
-
-    @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
+        return handleExceptionInternal(
+                ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request
+        );
     }
 
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+    public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
+        return handleExceptionInternal(
+                ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request
+        );
     }
+
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
+        return handleExceptionInternal(
+                ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request
+        );
+
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        if (body == null) {
+
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem(statusCode.toString())
+                    .build();
+        } else if (body instanceof String) {
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem((String) body)
+                    .build();
+        }
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
+
+
 }
