9ba8be8687bc346bbceb412dbd0a5c6ac569f3a6
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-03 19:15:42 -0300

    feat: JsonInclude para nao aceitar valores nulos

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/exceptionhandler/Problem.java
@@ -0,0 +1,17 @@
+package com.course.ais.io_algafood_api.api.exceptionhandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Builder //  O bulder é usado para criar instâncias de Problema de forma mais legível e flexível
+public class Problem {
+
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
+
+}
