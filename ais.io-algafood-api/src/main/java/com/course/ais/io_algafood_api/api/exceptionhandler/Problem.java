7c73022691c0b36acbcf96aec9c2426c92b45bec
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-04 13:22:35 -0300

    feat: novos atributos par a Problem

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/exceptionhandler/Problem.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/exceptionhandler/Problem.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Builder //  O bulder é usado para criar instâncias de Problema de forma mais legível e flexível
@@ -14,4 +16,6 @@ public class Problem {
     private String title;
     private String detail;
 
+    private String userMessage;
+    private LocalDateTime timestamp;
 }
