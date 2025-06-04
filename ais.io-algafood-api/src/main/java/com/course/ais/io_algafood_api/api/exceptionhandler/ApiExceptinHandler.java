894b167d74243ba874747b9f143bf245cfcebd04
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-04 16:01:23 -0300

    feat: metodo handleMethodArgumentNotValid

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/exceptionhandler/ApiExceptinHandler.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/exceptionhandler/ApiExceptinHandler.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -32,6 +33,20 @@ public class ApiExceptinHandler extends ResponseEntityExceptionHandler {
             = "Ocorreu um erro inesperado no sistema. "
             + "Tente novamente e se o problema persistir, entre em contato "
             + "com o administrador do sistema.";
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+        String detail = "Um ou mais campos estão inválidos. "
+                + "Faça o preenchimento correto e tente novamente.";
+
+        Problem problem = createProblemBuilder(HttpStatus.valueOf(status.value()), problemType, detail)
+                .userMessage(MSG_ERRO_GENERICO_USUARIO_FINAL)
+                .build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
