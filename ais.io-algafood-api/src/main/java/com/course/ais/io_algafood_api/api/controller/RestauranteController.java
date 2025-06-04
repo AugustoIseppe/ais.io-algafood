157827308a8de7f5915d26ce34b36824afb792b5
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-04 13:23:09 -0300

    feat: update no merge do metodo atualizar

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/controller/RestauranteController.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/controller/RestauranteController.java
@@ -1,15 +1,19 @@
 package com.course.ais.io_algafood_api.api.controller;
 
 import com.course.ais.io_algafood_api.domain.exceptions.CozinhaNaoEncontradaException;
-import com.course.ais.io_algafood_api.domain.exceptions.EntidadeNaoEncontradaException;
 import com.course.ais.io_algafood_api.domain.exceptions.NegocioException;
 import com.course.ais.io_algafood_api.domain.model.Restaurante;
 import com.course.ais.io_algafood_api.domain.repository.RestauranteRepository;
 import com.course.ais.io_algafood_api.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -62,31 +66,36 @@ public class RestauranteController {
 
     @PatchMapping("/{restauranteId}")
     public Restaurante atualizarParcial(@PathVariable Long
-                                                restauranteId, @RequestBody Map<String, Object> campos) {
+                                                restauranteId, @RequestBody Map<String, Object> campos, HttpServletRequest request) {
 
         Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
 
-        merge(campos, restauranteAtual.get());
+        merge(campos, restauranteAtual.get(), request);
         return atualizar(restauranteId, restauranteAtual.get());
     }
 
-    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+        ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+            Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 
-        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-            System.out.println("Propriedade: " + nomePropriedade + ", Valor: " + valorPropriedade);
-            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-            if (field != null) {
+            dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+                System.out.println("Propriedade: " + nomePropriedade + ", Valor: " + valorPropriedade);
+                Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
 
                 field.setAccessible(true);
                 Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
                 ReflectionUtils.setField(field, restauranteDestino, novoValor);
-            } else {
-                throw new IllegalArgumentException("Propriedade " + nomePropriedade + " não existe na classe Restaurante");
-            }
-        });
+
+            });
+        } catch (IllegalArgumentException e) {
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+        }
     }
 
     @DeleteMapping("/{restauranteId}")
