78ccb989dbe7ab2d2a4c7860ebcfb001182e1cc1
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-07 00:26:48 -0300

    feat: incluindo novos dtos para a api

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/CozinhaDTO.java
@@ -0,0 +1,11 @@
+package com.course.ais.io_algafood_api.api.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CozinhaDTO {
+    private Long id;
+    private String nome;
+}
