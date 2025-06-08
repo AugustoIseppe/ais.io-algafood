70b5dbd5d3a3630611ad1c1d2d6e28ef355fa86a
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-08 12:02:31 -0300

    refactor: alterando o nome da entidade que estava errada

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/exceptions/FormaPagamentoNaoEncontradaException.java
@@ -0,0 +1,15 @@
+package com.course.ais.io_algafood_api.domain.exceptions;
+
+public class FormaPagamentoNaoEncontradaException extends EntidadeNaoEncontradaException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FormaPagamentoNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+
+    public FormaPagamentoNaoEncontradaException(Long formaPagamentoId) {
+        this(String.format("Não existe um cadastro de forma de pagamento com código %d", formaPagamentoId));
+    }
+
+}
