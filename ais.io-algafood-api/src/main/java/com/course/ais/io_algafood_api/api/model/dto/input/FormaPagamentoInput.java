5a2ebc910ce5428a648d7b3f11bda531c6b69fbd
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-08 12:02:49 -0300

    feat: add not blank

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/input/FormaPagamentoInput.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/input/FormaPagamentoInput.java
@@ -1,10 +1,13 @@
 package com.course.ais.io_algafood_api.api.model.dto.input;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class FormaPagamentoInput {
+
+    @NotBlank
     private String descricao;
 }
