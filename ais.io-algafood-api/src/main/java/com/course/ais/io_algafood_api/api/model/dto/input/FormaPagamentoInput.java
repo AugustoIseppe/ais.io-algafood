744989aa9b15f48c96a8da6e1d14887080a31f4e
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-08 11:51:44 -0300

    feat: adicionando modelo de output/input para forma de pagamento

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/input/FormaPagamentoInput.java
@@ -0,0 +1,10 @@
+package com.course.ais.io_algafood_api.api.model.dto.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FormaPagamentoInput {
+    private String descricao;
+}
---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/output/FormaPagamentoModel.java
@@ -0,0 +1,11 @@
+package com.course.ais.io_algafood_api.api.model.dto.output;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FormaPagamentoModel {
+    private Long id;
+    private String descricao;
+}
