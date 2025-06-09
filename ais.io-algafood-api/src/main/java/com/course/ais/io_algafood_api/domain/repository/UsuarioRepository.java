dacb7d028c10ee44be78da55a86f6e80b47ddf8e
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-09 13:45:30 -0300

    feat: implementacao da regra de negocio para evitar usuarios com e-mails duplicados

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/UsuarioRepository.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/UsuarioRepository.java
@@ -4,7 +4,11 @@ import com.course.ais.io_algafood_api.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 
 }
--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/service/CadastroUsuarioService.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/service/CadastroUsuarioService.java
@@ -4,9 +4,12 @@ import com.course.ais.io_algafood_api.domain.exceptions.NegocioException;
 import com.course.ais.io_algafood_api.domain.exceptions.UsuarioNaoEncontradoException;
 import com.course.ais.io_algafood_api.domain.model.Usuario;
 import com.course.ais.io_algafood_api.domain.repository.UsuarioRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class CadastroUsuarioService {
@@ -14,8 +17,20 @@ public class CadastroUsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Transactional
     public Usuario salvar(Usuario usuario) {
+
+        entityManager.detach(usuario);
+
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+            throw new NegocioException("Já existe um usuário cadastrado com este e-mail.");
+        }
+
         return usuarioRepository.save(usuario);
     }
 
