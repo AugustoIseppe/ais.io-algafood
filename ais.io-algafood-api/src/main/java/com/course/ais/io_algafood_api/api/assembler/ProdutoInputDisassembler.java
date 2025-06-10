d1325e6d2dd8094dd5aa56c199c71145840fec53
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-10 10:36:56 -0300

    feat: implementacao de produtos

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/assembler/ProdutoInputDisassembler.java
@@ -0,0 +1,22 @@
+package com.course.ais.io_algafood_api.api.assembler;
+
+import com.course.ais.io_algafood_api.api.model.dto.input.ProdutoInput;
+import com.course.ais.io_algafood_api.domain.model.Produto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Produto toDomainObject(ProdutoInput produtoInput) {
+        return modelMapper.map(produtoInput, Produto.class);
+    }
+
+    public void copyToDomainObject(ProdutoInput produtoInput, Produto produto) {
+        modelMapper.map(produtoInput, produto);
+    }
+}
---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/service/CadastroProdutoService.java
@@ -0,0 +1,25 @@
+package com.course.ais.io_algafood_api.domain.service;
+
+import com.course.ais.io_algafood_api.domain.exceptions.ProdutoNaoEncontradoException;
+import com.course.ais.io_algafood_api.domain.model.Produto;
+import com.course.ais.io_algafood_api.domain.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CadastroProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Transactional
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+        return produtoRepository.findById(restauranteId, produtoId)
+                .orElseThrow(() -> new ProdutoNaoEncontradoException(restauranteId, produtoId));
+    }
+}
