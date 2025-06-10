de28d5f17e085918665ac88de5b4897f4f00fb7f
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-10 11:25:05 -0300

    feat: Permissao Model

---
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/output/PermissaoModel.java
@@ -0,0 +1,13 @@
+package com.course.ais.io_algafood_api.api.model.dto.output;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PermissaoModel {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+}
