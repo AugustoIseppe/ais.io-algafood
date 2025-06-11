ea4d93308e8f7252f025cf8eee530c98a863520c
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-11 15:21:19 -0300

    build: novo seeding - insere codigo no insert

--- ais.io-algafood-api/src/main/resources/db/migration/afterMigrate.sql
+++ ais.io-algafood-api/src/main/resources/db/migration/afterMigrate.sql
@@ -110,10 +110,10 @@ INSERT IGNORE INTO produto (id, nome, descricao, preco, ativo, restaurante_id) V
 
 INSERT INTO restaurante_usuario_responsavel (restaurante_id, usuario_id) VALUES (1, 5), (3, 5);
 
-insert into pedido (id, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
     endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
     status, data_criacao, subtotal, taxa_frete, valor_total)
-values (1, 1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
+values (1, '5c4be282-46cc-11f0-a919-74563c3a280c', 1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
 'CRIADO', utc_timestamp, 298.90, 10, 308.90);
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
