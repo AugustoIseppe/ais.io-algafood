4f4f37364e828d29dc897dc68db609893deb8be7
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-13 10:37:12 -0300

    feat: criando uma nova anotation para aceitar apenas fotos jpg ou png

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/input/FotoProdutoInput.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/input/FotoProdutoInput.java
@@ -1,10 +1,12 @@
 package com.course.ais.io_algafood_api.api.model.dto.input;
 
+import com.course.ais.io_algafood_api.core.validation.FileContentType;
 import com.course.ais.io_algafood_api.core.validation.FileSize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
@@ -13,6 +15,7 @@ public class FotoProdutoInput {
 
     @NotNull
     @FileSize(max = "500KB")
+    @FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
     private MultipartFile arquivo;
 
     @NotBlank
